b121bfbd6ed88639f5bd3bff88da6a8b9cc3e8cd
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-10-01 01:00:09 -0300

    Wrapper class to content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import "./global.css"
+import "./global.css";
+import styles from "./App.module.css";
 import { Header } from "./components/Header";
+import { Post } from "./Post";
+import { SideBar } from "./components/SideBar";
 
 export function App() {
   return (
     <>
       <Header />
-
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          <Post author="Samuel Adriano" content="Teste de posategem" />
+          <Post author="Samuel Adriano" content="Teste de posategem" />
+        </main>
+      </div>
     </>
   );
 }
