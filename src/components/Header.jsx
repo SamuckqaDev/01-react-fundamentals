2eb450fbdebc90369e9df5383a47ae489549ee0b
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-10-01 00:14:28 -0300

    header finished !

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import styles from "./Header.module.css";
+import logoImg from "../assets/ignite-logo.svg"
 
-console.log(styles)
+console.log(styles);
 
-export function Header(){
-    return(
-        <strong className={styles.header}>Ignite Feed</strong>
-    )
+export function Header() {
+  return (
+    <div className={styles.header}>
+        <img src={logoImg} alt="Ignite logotype" />
+      <strong>Ignite Feed</strong>
+    </div>
+  );
 }
