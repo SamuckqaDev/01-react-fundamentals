b0d154f03ab1f3815753fe12deb41907fa562009
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-10-01 01:58:38 -0300

    SideBar structure

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,23 @@
 import styles from "./SideBar.module.css";
+import profileImg from "../assets/profile-img.jpg";
 
-export function SideBar(){
-    return (
-        <aside className={styles.sidebar}>
-            
-        </aside>
-    )
+export function SideBar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=100"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <strong>Samuel Tomimatu</strong>
+        <span>FullStack developer</span>
+      </div>
+
+      <footer>
+        <a href="#">Edit profile</a>
+      </footer>
+    </aside>
+  );
 }
