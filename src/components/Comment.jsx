dc153abfa40421314c89283ef261fd31c05a2166
karinagramos <samuel.tomimatu@bandtec.com.br>
2022-10-25 00:02:26 -0300

    Comment component with stylized

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/SamuckqaDev.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Jhosias Severino</strong>
+              <time title="May 11 at 12:32 am" dateTime="2022-10-22">
+                Posted 1h ago
+              </time>
+            </div>
+            <button title="Delete comment">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Very good samuel, congratulations !</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Like<span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
