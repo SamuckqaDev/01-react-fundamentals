c3ad1f8affa0e15525767a43a2011281943df075
karinagramos <samuel.tomimatu@bandtec.com.br>
2022-10-28 11:56:50 -0300

    Post array created to render posts on screen

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,63 @@ import { Post } from "./components/Post";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrk: "http://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala Galera !" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir um novo projeto no meu portifa. Fiz no NlW da Rocketseat, muito maça, o nome é IgniteFeed.",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrk: "http://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala Galera !" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir um novo projeto no meu portifa. Fiz no NlW da Rocketseat, muito maça, o nome é IgniteFeed.",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrk: "http://github.com/samuckqadev.png",
+      name: "Samuel Tomimatu",
+      role: "QA Intern @CIP/SA",
+    },
+    content: [
+      { type: "paragraph", content: "Fala Galera !" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir um novo projeto no meu portifa. Fiz no NlW da Rocketseat, muito maça, o nome é IgniteFeed.",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+];
+
 export function App() {
   return (
     <>
@@ -12,8 +69,14 @@ export function App() {
       <div className={styles.wrapper}>
         <SideBar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </>
